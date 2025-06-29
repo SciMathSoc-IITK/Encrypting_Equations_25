f6a2a5a6e956498b47179abb4bbb5320445a4fc1
Shreyas Asthana <shreyas21.asthana@gmail.com>
2025-06-29 20:40:26 +0530

    Adding the DH Key Exchange cryptography in Cpp file
    This file contains the implimentation of the Diffie-Hellman Key Exchange in a Cpp file without use of any external libraries.

---
+++ Encryption Algorithms/String_Difflie-Hellmam_Key_Exchange.cpp
@@ -0,0 +1,448 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Chosing a Mod_Base and a primitive root.
+string Safe_Prime =
+    "3231700607131100730071487668866995196044410266971548403213034542752465510513970940663544946400061009953925634212968748340312864603338180186583201232845547"
+    "8866640049017814612630453339508537800140676854062458178866577153462154407416692817948320700932171155756949076498661479101692574856991514582183210728832907504"
+    "2090917655260315101013388913134224552143936251157248146915778812870284821807170018444513424751543209344508452506312723147061327626592232262108298527438201856"
+    "4037612346620576471766060589281211868572051549721520352075038855541814985919715060001583356111620029534316822120570056368765122712808350462125396031450425122"
+    "3312383348143970710437173613177513666117411349983735524526394526801714528369016880127098097958314790267552363653460486692568518498559280617036307194960711674"
+    "2606841051054143229655144449006254212059238890312033503230009652184505114302163879639239301281";
+
+//Note that the primitive root is small.
+string Power_Base = "2"; 
+
+
+//Declaring Functions
+string Normalize(string);
+int String_Compare(string, string);
+string String_Addition(string, string);
+string String_Subtraction(string, string);
+string String_Multiplication(string, string);
+string String_Divide(string, string);
+string String_Modulo(string, string);
+string String_GCD(string, string);
+string String_Modulo_Exponentiation(string, string, string);
+pair<string, pair<string, string>> String_Extended_GCD(string, string);
+string String_Modulo_Inverse(string, string);
+
+
+
+
+
+//Function for making the number usable, removing leading zeros.
+string Normalize(string s) {
+
+    //Checking if we have to add a negative sign at the end.
+    bool neg = false;
+    if (s[0] == '-') {
+        neg = true;
+        s = s.substr(1);
+    }
+
+    //Removing the leading zeros.
+    int i = 0;
+    while (i < s.size() - 1 && s[i] == '0') i++;
+    s = s.substr(i);
+
+    //adding the negative sign back.
+    if (neg && s != "0") s = '-' + s;
+    return s;
+}
+
+
+
+//This function returns -1,1 and 0 when s1 is less than, greater than and equal to s2 respectively.
+int String_Compare(string s1, string s2) {
+    //Remove the leading zeros.
+    s1 = Normalize(s1);
+    s2 = Normalize(s2);
+    
+    //Checking if either of them is negative.
+    bool neg1 = (s1[0] == '-');
+    bool neg2 = (s2[0] == '-');
+
+    //Basic sign check
+    if (neg1 && !neg2) return -1; 
+    if (!neg1 && neg2) return 1;  
+    
+    //If both are negative we invert the mod inequality
+    if (neg1 && neg2) {
+        s1 = s1.substr(1);
+        s2 = s2.substr(1);
+        if (s1.size() > s2.size()) return -1;
+        if (s1.size() < s2.size()) return 1;
+        if (s1 > s2) return -1;
+        if (s1 < s2) return 1;
+        return 0;
+    }
+
+    //If both positive we just compare them.
+    if (s1.size() > s2.size()) return 1;
+    if (s1.size() < s2.size()) return -1;
+    if (s1 > s2) return 1;
+    if (s1 < s2) return -1;
+    return 0;
+}
+
+
+
+//Function for subtracting the strings.
+string String_Subtraction(string s1, string s2) {
+    //Making number usable
+    s1 = Normalize(s1);
+    s2 = Normalize(s2);
+    
+    //Checking the sign of each of the numbers
+    bool neg1 = (s1[0] == '-');
+    bool neg2 = (s2[0] == '-');
+    
+    //Checking whether the difference will be positive or negative and calling functions accordingly.
+    if (neg1 && !neg2) return '-' + String_Addition(s1.substr(1), s2);
+    if (!neg1 && neg2) return String_Addition(s1, s2.substr(1));
+    if (neg1 && neg2)  return String_Subtraction(s2.substr(1), s1.substr(1));
+    if (String_Compare(s1, s2) < 0) return '-' + String_Subtraction(s2, s1);
+
+    // Actual subtraction
+    reverse(s1.begin(), s1.end());
+    reverse(s2.begin(), s2.end());
+    
+    //Making the length of both numbers same. (Note s1.size() >= s2.size())
+    while (s2.size() < s1.size()) s2 += '0';
+    
+    //Initializing variables
+    string result = "";
+    int borrow = 0;
+    
+    //Implimenting the normal subtraction and borrow.
+    for (int i = 0; i < s1.size(); i++) {
+
+        //Storing digits.
+        int digit1 = s1[i] - '0';
+        int digit2 = (i < s2.size()) ? s2[i] - '0' : 0;
+        
+        //Borrow implimentation.
+        int diff = digit1 - digit2 - borrow;
+        if (diff < 0) {
+            diff += 10;
+            borrow = 1;
+        } else {
+            borrow = 0;
+        }
+        result += (diff + '0');
+    }
+    
+    // Remove leading zeros
+    while (result.size() > 1 && result.back() == '0') {
+        result.pop_back();
+    }
+    
+    //Reverting the number back.
+    reverse(result.begin(), result.end());
+    return Normalize(result);
+}
+
+
+
+//Function for adding the strings.
+string String_Addition(string s1, string s2) {
+    //Making the numbers usable.
+    s1 = Normalize(s1);
+    s2 = Normalize(s2);
+    
+    //Checking the sign of the numbers.
+    bool neg1 = (s1[0] == '-');
+    bool neg2 = (s2[0] == '-');
+    
+    //Checking the sign of the sum and calling functions accordingly.
+    if (neg1 && !neg2) return String_Subtraction(s2, s1.substr(1));
+    if (!neg1 && neg2) return String_Subtraction(s1, s2.substr(1));
+    if (neg1 && neg2) return '-' + String_Addition(s1.substr(1), s2.substr(1));
+
+    // Actual addition algorithm.
+    reverse(s1.begin(), s1.end());
+    reverse(s2.begin(), s2.end());
+    
+    //Making the size of both the numbers equal.
+    int n = max(s1.size(), s2.size());
+    while (s1.size() < n) s1 += '0';
+    while (s2.size() < n) s2 += '0';
+    
+    //Initializing variables
+    string result = "";
+    int carry = 0;
+    
+    //Implimentation of digit-wise sum and carry.
+    for (int i = 0; i < n; i++) {
+        int sum = (s1[i] - '0') + (s2[i] - '0') + carry;
+        result += (sum % 10 + '0');
+        carry = sum / 10;
+    }
+    if (carry) result += (carry + '0');
+    
+    //Reverting the number back.
+    reverse(result.begin(), result.end());
+    return Normalize(result);
+}
+
+
+
+//Function of multipying the strings.
+string String_Multiplication(string s1, string s2) {
+    //Making the numbers useful.
+    s1 = Normalize(s1);
+    s2 = Normalize(s2);
+    
+    //If ither one is zero product will be zero.
+    if (s1 == "0" || s2 == "0") return "0";
+    
+    //Checking sign of the numbers
+    bool neg1 = (s1[0] == '-');
+    bool neg2 = (s2[0] == '-');
+
+    //Checking sign of the product.
+    bool result_negative = (neg1 != neg2);
+    
+    //Taking the absolute value.
+    if (neg1) s1 = s1.substr(1);
+    if (neg2) s2 = s2.substr(1);
+    
+    //Initializing Variables.
+    int n = s1.size(), m = s2.size();
+    vector<int> result(n + m, 0);
+    
+    //Actual multiplication algorithm
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = m - 1; j >= 0; j--) {
+            int mul = (s1[i] - '0') * (s2[j] - '0');
+            int sum = mul + result[i + j + 1];
+            result[i + j + 1] = sum % 10;
+            result[i + j] += sum / 10;
+        }
+    }
+    
+    //Changing the answer back into a string.
+    string ans = "";
+    bool leading_zero = true;
+    for (int digit : result) {
+        if (digit == 0 && leading_zero) continue;
+        leading_zero = false;
+        ans += (digit + '0');
+    }
+
+    if (ans.empty()) ans = "0";
+    if (result_negative && ans != "0") ans = '-' + ans;
+    return Normalize(ans);
+}
+
+
+
+//Function for diving the strings. Note that this function does integer division, that is it will not return fractions or decimals.
+string String_Divide(string dividend, string divisor) {
+    //Making the numbers usable.
+    dividend = Normalize(dividend);
+    divisor = Normalize(divisor);
+    
+    //Division by zero is not possible but for simplicity here it just returns 0;
+    if (divisor == "0") return "0"; 
+    if (dividend == "0") return "0";
+    
+    //Checking the sign of numbers and the quotient.
+    bool neg_dividend = (dividend[0] == '-');
+    bool neg_divisor = (divisor[0] == '-');
+    bool result_negative = (neg_dividend != neg_divisor);
+    
+    //Taking the absolute value of the number.
+    if (neg_dividend) dividend = dividend.substr(1);
+    if (neg_divisor) divisor = divisor.substr(1);
+    
+    //No fractions in integer division.
+    if (String_Compare(dividend, divisor) < 0) return "0";
+    
+    //Initializing Variables
+    string result = "";
+    string current = "";
+    
+    //Actual Division algorithm.
+    for (int i = 0; i < dividend.size(); i++) {
+        current += dividend[i];
+        current = Normalize(current);
+        
+        int quotient_digit = 0;
+        while (String_Compare(current, divisor) >= 0) {
+            current = String_Subtraction(current, divisor);
+            quotient_digit++;
+        }
+        
+        result += (quotient_digit + '0');
+    }
+    
+    // Remove leading zeros
+    int pos = 0;
+    while (pos < result.size() - 1 && result[pos] == '0') pos++;
+    result = result.substr(pos);
+    
+    if (result_negative && result != "0") result = '-' + result;
+    
+    return Normalize(result);
+}
+
+
+
+//Function for calculating the modulus residue of s1 with respect to s2, which is usually represented as s1%s2.
+string String_Modulo(string dividend, string divisor) {
+    //Making numbers useable.
+    dividend = Normalize(dividend);
+    divisor = Normalize(divisor);
+    
+    //Again division by 0 results in a return value of 0 for simplicity.
+    if (divisor == "0") return "0"; 
+    if (dividend == "0") return "0";
+    
+    //Checking sign of numbers
+    bool neg_dividend = (dividend[0] == '-');
+    bool neg_divisor = (divisor[0] == '-');
+    
+    //Taking absolute value
+    if (neg_dividend) dividend = dividend.substr(1);
+    if (neg_divisor) divisor = divisor.substr(1);
+    
+    //If the mod of s1 is less than s2.
+    if (String_Compare(dividend, divisor) < 0) return neg_dividend ? '-' + dividend : dividend;
+    
+    //Same as the human way of solving the remainder.
+    string quotient = String_Divide(dividend, divisor);
+    string product = String_Multiplication(quotient, divisor);
+    string remainder = String_Subtraction(dividend, product);
+    
+    //Making remainder positive, just in case.
+    if (neg_dividend && remainder != "0") remainder = String_Subtraction(divisor, remainder);
+    
+    return Normalize(remainder);
+}
+
+
+
+//Function for finding GCD of the strings
+string String_GCD(string s1, string s2) {
+
+    //Normal Euclidian GCD algorithm.
+    if (Normalize(s2) == "0") return Normalize(s1);
+    return String_GCD(s2, String_Modulo(s1, s2));
+}
+
+
+
+//Function for calculating modular exponent.{Value = (base)^exponent (Mod mod)}
+string String_Modulo_Exponentiation(string base, string exponent, string mod) {
+
+    //Initializing variables.
+    string result = "1";
+    base = String_Modulo(base, mod);
+
+    //Usual implimentation of the interative Binary Exponentiation.
+    while (String_Compare(exponent, "0")) {
+        if(exponent == "0") break;
+        if ((exponent.back() - '0') % 2 == 1) result = String_Modulo(String_Multiplication(result, base), mod);
+        base = String_Modulo(String_Multiplication(base, base), mod);
+        exponent = String_Divide(exponent, "2");
+    }
+
+    //Returning the value.
+    return result;
+}
+
+
+
+//Function returning the coeffeciets of the Bezout's Lemma. (ax + by = GCD(a,b))
+pair<string, pair<string, string>> String_Extended_GCD(string a, string b) {
+
+    //Making numbers usable.
+    a = Normalize(a);
+    b = Normalize(b);
+    
+    //Initializing variables.
+    string old_r = a, r = b;
+    string old_s = "1", s = "0";
+    string old_t = "0", t = "1";
+    
+    //Using the common Bezout's recursive relation to solve the problm iteratively.
+    while (r != "0") {
+        string quotient = String_Divide(old_r, r);
+        
+        string new_r = String_Subtraction(old_r, String_Multiplication(quotient, r));
+        old_r = r;
+        r = new_r;
+        
+        string new_s = String_Subtraction(old_s, String_Multiplication(quotient, s));
+        old_s = s;
+        s = new_s;
+        
+        string new_t = String_Subtraction(old_t, String_Multiplication(quotient, t));
+        old_t = t;
+        t = new_t;
+    }
+    
+    return make_pair(old_r, make_pair(old_s, old_t));
+}
+
+
+
+// Calculating the multiplicative inverse of a number in the set of all modular residues of another number.
+string String_Modulo_Inverse(string a, string mod) {
+
+    //Making numbers usable.
+    a = Normalize(a);
+    mod = Normalize(mod);
+    
+    //Intilializing variables.
+    auto result = String_Extended_GCD(a, mod);
+    string gcd = result.first;
+    string x = result.second.first;
+    
+    // If they arent coprime, no inverse exists.
+    if (gcd != "1") return 0;
+    
+    // Make sure x is positive and less than mod.
+    x = Normalize(x);
+    while (x[0] == '-') {
+        x = String_Addition(x, mod);
+    }
+    x = String_Modulo(x, mod);
+    
+    return Normalize(x);
+}
+
+
+
+
+int main(){
+
+    //Initiallizing Variables
+    string Secret_Key ;
+    string Key1,Key2;
+
+    //Inputting the keys
+    cin >> Key1 >> Key2;
+    string Message1="1",Message2="1";
+
+    //Calculating the messages
+    Message1 = String_Modulo_Exponentiation(Power_Base,Key1,Safe_Prime);
+    Message2 = String_Modulo_Exponentiation(Power_Base,Key2,Safe_Prime);
+    Secret_Key = String_Modulo_Exponentiation(Message1,Key2,Safe_Prime);
+
+    //Printing the keys
+    cout << "First person chose the key \n" << Key1 <<endl;
+    cout << "Second person chose the key \n" << Key2 <<endl;
+    //Printing the messages
+    cout<<"First person sent message \n";cout << Message1 <<endl;
+    cout<<"Second person sent message \n";cout << Message2 <<endl;
+    //Printing the final secret message
+    cout << "Final Secret Key is \n";cout << Secret_Key;
+    return 0;
+}
+
+
+
